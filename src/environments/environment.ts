e2606c4bf5f78a481a24ece6205405092bd457ef
EricMOB-afk <U202315032@upc.edu.pe>
2025-05-11 21:35:58 -0500

    feat: add components to start front

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  // EJEMPLO SIEMPRE VER TU BASE PATH Y ENDPOINT
+  serverBasePath: 'http://localhost:3000/api/v1',
+  studentsEndpointPath: '/students'
+  };
