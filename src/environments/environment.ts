d1b0321137522c17b55c58362775caf3168a7734
Walter <u202221632@upc.edu.pe>
2025-05-13 15:10:07 -0500

    feat: changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // EJEMPLO SIEMPRE VER TU BASE PATH Y ENDPOINT
+
   serverBasePath: 'https://ca5bc3786cf054e4e249.free.beeceptor.com/api/v1',
   studentsEndpointPath: '/students'
   };
