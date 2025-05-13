fd044c95ab36a94da158549e1e0217526d402efb
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-05-13 16:53:17 -0500

    feat: add careers-dialog component.

---
+++ src/app/student-dashboard/components/career-dialog/career-dialog.component.ts
@@ -0,0 +1,28 @@
+import {Component, Inject} from '@angular/core';
+
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Career } from '../../model/career.entity';
+import {TranslatePipe} from '@ngx-translate/core';
+import {MatButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-career-dialog',
+  imports: [
+    TranslatePipe,
+    MatIcon,
+    MatButton
+  ],
+  templateUrl: './career-dialog.component.html',
+  styleUrl: './career-dialog.component.css'
+})
+export class CareerDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<CareerDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { careers: Career[] }
+  ) {}
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+}
