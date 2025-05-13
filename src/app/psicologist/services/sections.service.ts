5038ea077508a046a4d202588cf7bb2156ba12d9
Phatogram <todo_xqteuiero@outlook.com>
2025-05-13 17:56:57 -0500

    Create sections.service.ts

---
+++ src/app/psicologist/services/sections.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { BaseService } from "../../shared/services/base.service";
+import { HttpClient } from "@angular/common/http";
+import { Sections } from "../model/sections.entity";
+@Injectable({
+  providedIn: 'root'
+})
+export class SectionsService  extends BaseService<Sections> {
+
+  constructor(http: HttpClient) {
+    super(http);
+    this.resourceEndpoint = '/students';
+  }
+}
