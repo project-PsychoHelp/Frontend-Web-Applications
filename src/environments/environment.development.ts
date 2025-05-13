0cba6205194105d40e3bf02d5c0ea619f0be8cd4
EricMOB-afk <U202315032@upc.edu.pe>
2025-05-13 14:58:25 -0500

    feat: update css component

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,11 @@
 export const environment = {
   production: false,
   // EJEMPLO SIEMPRE VER TU BASE PATH Y ENDPOINT
-  serverBasePath: 'https://ca5d0456426a6b35d2e6.free.beeceptor.com/api/v1',
+
+  serverBasePath: 'https://ca4e4ca78ca1be8591fe.free.beeceptor.com/api/v1',
+
+  //serverBasePath: 'https://ca881566554f71492d5b.free.beeceptor.com/api/v1',
+  //serverBasePath: 'https://ca5d0456426a6b35d2e6.free.beeceptor.com/api/v1',
   //serverBasePath: 'http://localhost:3000/api/v1',
   testsEndpointPath: '/tests',
   };
