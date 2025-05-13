f46d0ca80f19b211848d3fb3c90fa663aa74b98c
JoaquinC <u20201f788@upc.edu.pe>
2025-05-13 13:30:51 -0500

    Añadiendo rama feature-PsychologistSearch 13/05/2025

---
+++ src/app/PsychologistSearch/pages/psychologist-managment-component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { PsychologistService } from '../../services/psychologists.service';
+import { Psychologist } from '../../model/psychologist.entity';
+
+@Component({
+  selector: 'app-psychologist-management',
+  templateUrl: './psychologist-managment-component.html',
+  styleUrls: ['./psychologist-managment-component.css']
+})
+export class PsychologistManagementComponent implements OnInit, AfterViewInit {
+
+  psychologistData: Psychologist = new Psychologist();
+  dataSource!: MatTableDataSource<Psychologist>;
+  displayedColumns: string[] = ['id', 'name', 'age', 'email', 'experiencie', 'speciality', 'actions'];
+  isEditMode: boolean = false;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private psychologistService: PsychologistService) {}
+
+  ngOnInit(): void {
+    this.getAllPsychologists();
+  }
+
+  ngAfterViewInit(): void {
+    if (this.dataSource) {
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    }
+  }
+
+  private getAllPsychologists(): void {
+    this.psychologistService.getAll().subscribe((data: Psychologist[]) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  private resetEditState(): void {
+    this.psychologistData = new Psychologist();
+    this.isEditMode = false;
+  }
+
+  private createPsychologist(): void {
+    this.psychologistService.create(this.psychologistData).subscribe(() => {
+      this.getAllPsychologists();
+      this.resetEditState();
+    });
+  }
+
+  private updatePsychologist(): void {
+    this.psychologistService.update(this.psychologistData).subscribe(() => {
+      this.getAllPsychologists();
+      this.resetEditState();
+    });
+  }
+
+  private deletePsychologist(id: number): void {
+    this.psychologistService.delete(id).subscribe(() => {
+      this.getAllPsychologists();
+    });
+  }
+
+  onEditItem(element: Psychologist) {
+    this.psychologistData = { ...element };
+    this.isEditMode = true;
+  }
+
+  onDeleteItem(element: Psychologist) {
+    if (confirm(`¿Eliminar al psicólogo ${element.name}?`)) {
+      this.deletePsychologist(element.id);
+    }
+  }
+
+  onCancelEdit() {
+    this.resetEditState();
+  }
+
+  onSubmitForm() {
+    this.isEditMode ? this.updatePsychologist() : this.createPsychologist();
+  }
+}
