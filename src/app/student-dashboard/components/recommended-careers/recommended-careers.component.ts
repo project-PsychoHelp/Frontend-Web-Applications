32245f32b415c72b31daa596ed414eba387564aa
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-05-13 23:00:17 -0500

    chore: add error handling in ngOnInit() in case student data can not be fetched.

--- src/app/student-dashboard/components/recommended-careers/recommended-careers.component.ts
+++ src/app/student-dashboard/components/recommended-careers/recommended-careers.component.ts
@@ -2,12 +2,13 @@ import {Component, OnInit} from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 import { Student } from '../../model/student.entity';
-import {NgIf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import {MatButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {TranslatePipe} from '@ngx-translate/core';
 
 import { MatDialog } from '@angular/material/dialog';
+
 import {CareerDialogComponent} from '../career-dialog/career-dialog.component';
 
 @Component({
@@ -16,23 +17,29 @@ import {CareerDialogComponent} from '../career-dialog/career-dialog.component';
     MatIcon,
     NgIf,
     MatButton,
-    TranslatePipe
+    TranslatePipe,
+    NgForOf
   ],
   templateUrl: './recommended-careers.component.html',
   styleUrl: './recommended-careers.component.css'
 })
 export class RecommendedCareersComponent implements OnInit {
   student: Student | null = null;
-  private dialog: any;
 
-  constructor(private http: HttpClient) {}
+
+  constructor(private http: HttpClient, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     // Fetch student data (using ID 1 for now, same as ProgressComponent)
     this.http.get<{ students: any[] }>('http://localhost:3000/db')
-      .subscribe(data => {
+      .subscribe({next:
+      data => {
         const studentData = data.students.find(s => s.id === 1);
         this.student = studentData ? new Student(studentData) : null;
+      },
+        error: err => {
+        console.error('Error fetching student data: ', err);
+        }
       });
   }
   openCareerDialog(): void {
@@ -43,6 +50,4 @@ export class RecommendedCareersComponent implements OnInit {
       });
     }
   }
-
-
 }
