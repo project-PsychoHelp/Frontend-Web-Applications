24c6bd54578bfec6b8d5bff8941fb0f681af1607
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-05-13 23:01:00 -0500

    fix: update imports with NgForOf.

--- src/app/student-dashboard/components/career-dialog/career-dialog.component.ts
+++ src/app/student-dashboard/components/career-dialog/career-dialog.component.ts
@@ -2,16 +2,19 @@ import {Component, Inject} from '@angular/core';
 
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Career } from '../../model/career.entity';
+
 import {TranslatePipe} from '@ngx-translate/core';
 import {MatButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-career-dialog',
   imports: [
     TranslatePipe,
     MatIcon,
-    MatButton
+    MatButton,
+    NgForOf
   ],
   templateUrl: './career-dialog.component.html',
   styleUrl: './career-dialog.component.css'
