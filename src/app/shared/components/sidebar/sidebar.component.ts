c958a88eedb3d2c278b0056747183a2e92a25ee7
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-05-13 23:56:53 -0500

    feat: add sidebar component.

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import {MatListItem, MatListItemIcon, MatNavList} from '@angular/material/list';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import {MatLine} from '@angular/material/core';
+import {MatIcon} from '@angular/material/icon';
+import {TranslatePipe} from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [
+    MatNavList,
+    MatIcon,
+    MatLine,
+    MatListItem,
+    RouterLinkActive,
+    MatListItemIcon,
+    RouterLink,
+    TranslatePipe,
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+  navItems = [
+    { label: 'SIDEBAR.DASHBOARD', route: '/dashboard', icon: 'dashboard' },
+    { label: 'SIDEBAR.TESTS', route: '/tests', icon: 'assignment' },
+    { label: 'SIDEBAR.CAREERS', route: '/careers', icon: 'school' },
+    { label: 'SIDEBAR.PROFILE', route: '/profile', icon: 'person' }
+  ];
+}
