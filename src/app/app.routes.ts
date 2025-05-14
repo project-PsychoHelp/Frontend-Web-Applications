c6232c123fd332405ac5b60be395e0da44325ed9
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-05-13 23:55:25 -0500

    feat: add routes to app.routes.ts.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {ProgressComponent} from './student-dashboard/components/progress/progress.component';
+import {
+  RecommendedCareersComponent
+} from './student-dashboard/components/recommended-careers/recommended-careers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: ProgressComponent}, //Dashboard shows the ProgressComponent
+  {path: 'careers', component: RecommendedCareersComponent}, //Careers route
+  {path: 'tests', component: ProgressComponent}, //Placeholder for tests
+  {path: 'profile', component: ProgressComponent}, //Placeholder for Profile
+
+
+];
