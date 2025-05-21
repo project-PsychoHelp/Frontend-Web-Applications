ffccde5dd6ef8e5d67ce530d83cc07761744c8a7
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-05-21 07:24:34 -0500

    feat: add more routes for the sidebar to app.routes.ts.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,17 @@ import {ProgressComponent} from './student-dashboard/components/progress/progres
 import {
   RecommendedCareersComponent
 } from './student-dashboard/components/recommended-careers/recommended-careers.component';
+import {PageNotFoundComponent} from './public/pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: ProgressComponent}, //Dashboard shows the ProgressComponent
-  {path: 'careers', component: RecommendedCareersComponent}, //Careers route
+  {path: 'careers', component: ProgressComponent}, //Careers route
   {path: 'tests', component: ProgressComponent}, //Placeholder for tests
   {path: 'profile', component: ProgressComponent}, //Placeholder for Profile
-
+  {path: 'sessions', component: ProgressComponent}, //Placeholder for sessions
+  {path: 'resources', component: ProgressComponent}, //Placeholder for resources
+  {path: 'settings', component: ProgressComponent}, //Placeholder for settings
+  {path: '**', component: PageNotFoundComponent}
 
 ];
