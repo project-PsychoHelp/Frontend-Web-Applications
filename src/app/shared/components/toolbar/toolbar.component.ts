bcc0fd43ed6559111dbb7823ca9b746185e330ec
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-05-21 07:27:52 -0500

    fix: implement the full name of the student in the toolbar fetched from db.json.

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import {MatToolbar} from '@angular/material/toolbar';
 import {TranslatePipe} from '@ngx-translate/core';
 import {MatIcon} from '@angular/material/icon';
@@ -17,10 +18,27 @@ import {LanguageSwitcherComponent} from '../../../public/components/language-swi
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
   @Output() toggleSidenav = new EventEmitter<void>();
+  studentName: string | null = null;
+
+  constructor(private http: HttpClient) {}
 
   onToggleSidenav() : void {
     this.toggleSidenav.emit();
+  }
+
+  ngOnInit():void {
+    this.http.get<{ students: any[] }>('http://localhost:3000/db')
+      .subscribe({
+        next: data => {
+          const studentData = data.students.find(s => s.id === 1);
+          this.studentName = studentData ? studentData.fullName : 'Unknown User';
+        },
+        error: err => {
+          console.error('Error fetching the data: ',err);
+          this.studentName = 'Unknown User';
+        }
+      })
   }
 }
