ae4ab192fea63252f1c60d5aa7688dfc6d96c3ff
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-06-22 21:02:57 -0500

    feat(panel-psychologist): minor updates.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,13 @@ import {
   RecommendedCareersComponent
 } from './student-dashboard/components/recommended-careers/recommended-careers.component';
 import {PageNotFoundComponent} from './public/pages/page-not-found/page-not-found.component';
+import {SectionsManagementComponent} from './psicologist/pages/sections-managament/sections-managament.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: 'about', component: PageNotFoundComponent },
+  { path: 'psicologist/sections', component: SectionsManagementComponent },
+  { path: 'psicologist/', component: SectionsManagementComponent},
   {path: 'dashboard', component: ProgressComponent}, //Dashboard shows the ProgressComponent
   {path: 'careers', component: ProgressComponent}, //Careers route
   {path: 'tests', component: ProgressComponent}, //Placeholder for tests
@@ -16,16 +20,5 @@ export const routes: Routes = [
   {path: 'settings', component: ProgressComponent}, //Placeholder for settings
   {path: '**', component: PageNotFoundComponent}
 
-];
-import { PageNotFoundComponent } from "./public/pages/page-not-found/page-not-found.component";
-import {SectionsManagementComponent} from './psicologist/pages/sections-managament/sections-managament.component';
-
-export const routes: Routes = [
-
-  { path: 'about', component: PageNotFoundComponent },
-  { path: 'psicologist/sections', component: SectionsManagementComponent },
-  { path: '', redirectTo: 'psicologist/sections', pathMatch: 'full' },
-  { path: 'psicologist/sections', component: SectionsManagementComponent},
-  { path: '**', component: PageNotFoundComponent }
 ];
 
