61910a60fa120c186714cbc9b81c61d0b242c618
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-06-23 00:54:05 -0500

    fix(environment): refactor serverBasePath.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
-  // EJEMPLO SIEMPRE VER TU BASE PATH Y ENDPOINT
-  serverBasePath: 'http://localhost:3000/api/v1',
+  serverBasePath: 'http://localhost:3000',
   studentsEndpointPath: '/students',
   sectionsEndpointPath: '/sections'
   };
