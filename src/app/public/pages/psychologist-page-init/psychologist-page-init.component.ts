2d3ef82222d0c297d56da949222f7afacaad9b62
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-06-23 06:35:33 -0500

    fix(psychologist-home-page): refactor texts.

---
+++ src/app/public/pages/psychologist-page-init/psychologist-page-init.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TranslatePipe} from '@ngx-translate/core';
+@Component({
+  selector: 'app-psychologist-page-init',
+  imports: [
+    TranslatePipe
+  ],
+  templateUrl: './psychologist-page-init.component.html',
+  styleUrl: './psychologist-page-init.component.css'
+})
+export class PsychologistPageInitComponent {
+
+}
