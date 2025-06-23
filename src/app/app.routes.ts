a237f243dd4a935f05a2234195baf592b51ba203
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-06-22 22:26:11 -0500

    feat(routes): refactor routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,15 +9,23 @@ import {SectionsManagementComponent} from './psicologist/pages/sections-managame
 export const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   { path: 'about', component: PageNotFoundComponent },
-  { path: 'psicologist/sections', component: SectionsManagementComponent },
-  { path: 'psicologist/', component: SectionsManagementComponent},
-  {path: 'dashboard', component: ProgressComponent}, //Dashboard shows the ProgressComponent
-  {path: 'careers', component: ProgressComponent}, //Careers route
-  {path: 'tests', component: ProgressComponent}, //Placeholder for tests
-  {path: 'profile', component: ProgressComponent}, //Placeholder for Profile
-  {path: 'sessions', component: ProgressComponent}, //Placeholder for sessions
-  {path: 'resources', component: ProgressComponent}, //Placeholder for resources
-  {path: 'settings', component: ProgressComponent}, //Placeholder for settings
+  { path: 'student',
+  children: [
+    {path: 'dashboard', component: ProgressComponent}, //Dashboard shows the ProgressComponent
+    {path: 'careers', component: ProgressComponent}, //Careers route
+    {path: 'tests', component: ProgressComponent}, //Placeholder for tests
+    {path: 'profile', component: ProgressComponent}, //Placeholder for Profile
+    {path: 'sessions', component: ProgressComponent}, //Placeholder for sessions
+    {path: 'resources', component: ProgressComponent}, //Placeholder for resources
+    {path: 'settings', component: ProgressComponent}, //Placeholder for settings
+  ]
+  },
+  {path: 'psychologist',
+  children: [
+    { path: '', component: PageNotFoundComponent },
+    { path: 'sections', component: SectionsManagementComponent },
+    { path: '**', component: PageNotFoundComponent }
+  ]},
   {path: '**', component: PageNotFoundComponent}
 
 ];
