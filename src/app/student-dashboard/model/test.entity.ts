da45ca2a3df777fed73267554845088183711ff5
VRojas1603 <rojas.vmanuel16@gmail.com>
2025-06-22 19:55:00 -0500

    feat(student-dashboard): minor updates.

--- src/app/student-dashboard/model/test.entity.ts
+++ src/app/student-dashboard/model/test.entity.ts
@@ -1,12 +1,14 @@
 export class Test {
   name: string;
-  status: 'Completado' | 'Pendiente';
+  //status: 'Completado' | 'Pendiente';
+  status: string;
   completionDate?: string;
   estimatedTime?: string;
 
   constructor(data: Partial<Test>) {
     this.name = data.name || '';
-    this.status = data.status || 'Pendiente';
+    //this.status = data.status || 'Pendiente';
+    this.status = data.status || '';
     this.completionDate = data.completionDate;
     this.estimatedTime = data.estimatedTime;
   }
